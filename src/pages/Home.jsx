846f76febb4682ccc7efadb30024d1af683a815a
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2023-06-30 09:29:28 +0100

    invoke the main map

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import Container from '../components/Container'
+import Main from '../map/Main'
 
 const Home = () => {
-  return <Container>Hello</Container>
+  return (
+    <Container>
+      <Main />
+    </Container>
+  )
 }
 
 export default Home
