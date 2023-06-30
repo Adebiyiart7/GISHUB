819a2be3f3bfe7ca095ae8dce04e218a741e8e45
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2023-06-30 20:45:25 +0100

    Created a basemap options object

---
+++ src/config/basemapOptions.js
@@ -0,0 +1,14 @@
+const basemaps = {
+  STREETS: 'streets',
+  TOPOGRAPHIC: 'topo',
+  IMAGERY: 'satellite',
+  NATIONAL_GEOGRAPHIC: 'national-geographic',
+  OCEANS: 'oceans',
+  LIGHT_GRAY_CANVAS: 'gray',
+  DARK_GRAY_CANVAS: 'dark-gray',
+  TERRAIN_WITH_LABELS: 'terrain',
+  OPENSTREETMAP: 'osm',
+  IMAGERY_WITH_LABELS: 'hybrid',
+}
+
+export default basemaps
