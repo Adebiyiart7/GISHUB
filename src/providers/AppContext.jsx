a1df295f3db3f858d74dccf05f2e3f49092e6d5f
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2023-06-30 09:29:02 +0100

    Created a context API for handling what left menu displays

---
+++ src/providers/AppContext.jsx
@@ -0,0 +1,47 @@
+import React, { createContext, useReducer } from 'react'
+import leftMenuContentTitles from '../config/leftMenuContentTitles'
+import FindData from '../components/FindData'
+import UploadData from '../components/UploadData'
+
+// Define your initial state
+const initialState = {
+  leftMenuContent: {
+    title: leftMenuContentTitles.FIND_DATA,
+    content: <FindData title={leftMenuContentTitles.FIND_DATA.title} />,
+  },
+}
+
+// Define your reducer function
+const reducer = (state, action) => {
+  switch (action.type) {
+    case leftMenuContentTitles.FIND_DATA._id:
+      return {
+        ...state,
+        leftMenuContent: {
+          title: leftMenuContentTitles.FIND_DATA,
+          content: <FindData title={leftMenuContentTitles.FIND_DATA.title} />,
+        },
+      }
+    case leftMenuContentTitles.UPLOAD_DATA._id:
+      return {
+        ...state,
+        leftMenuContent: {
+          title: leftMenuContentTitles.UPLOAD_DATA,
+          content: <UploadData title={leftMenuContentTitles.UPLOAD_DATA.title} />,
+        },
+      }
+
+    default:
+      return state
+  }
+}
+
+// Create the context
+export const AppContext = createContext()
+
+// Create the context provider
+export const AppContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return <AppContext.Provider value={{ state, dispatch }}>{children}</AppContext.Provider>
+}
