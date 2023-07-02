8aea0c10acb8fbc9a5cbc21029f68232b27bf89e
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2023-07-02 07:53:08 +0100

    Created a file card component

---
+++ src/components/left-menu/FileCard.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { BsFileEarmarkZip, BsInfoCircle } from 'react-icons/bs'
+import { MdOutlineFileDownload } from 'react-icons/md'
+import { VscMapFilled } from 'react-icons/vsc'
+
+const FileCard = ({ title, description, size }) => {
+  return (
+    <div className='flex flex-col gap-2 relative p-2 border rounded bg-white w-[268px] mx-2 hover:border-primary'>
+      <BsInfoCircle className='absolute top-2 cursor-pointer right-2 text-green-800' />
+      <div className='flex gap-2 items-center'>
+        <BsFileEarmarkZip size={35} className='text-gray-600' />
+        <div>
+          <div className='text-sm line-clamp-1 font-semibold'>{title}</div>
+          <div className='text-xs line-clamp-1 text-gray-600'>{description}</div>
+        </div>
+      </div>
+      <div className='flex justify-between items-center'>
+        <div className='flex items-center gap-2'>
+          <span className='flex items-center gap-1 text-xs border rounded font-semibold text-gray-600 px-2 py-1 cursor-pointer hover:border-primary hover:text-primary'>
+            <VscMapFilled size={20} /> Preview
+          </span>
+          <span className='flex items-center gap-1 text-xs border rounded font-semibold text-primary px-2 py-1 cursor-pointer bg-primary border-primary text-white'>
+            <MdOutlineFileDownload size={20} /> Download
+          </span>
+        </div>
+        <span className='text-xs text-gray-500 font-semibold uppercase'>{size}</span>
+      </div>
+    </div>
+  )
+}
+
+export default FileCard
