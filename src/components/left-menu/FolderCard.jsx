f83ce4baa2e71243d9a16311e4cb1dba65c12add
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2023-07-02 07:52:35 +0100

    Created a folder card component

---
+++ src/components/left-menu/FolderCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { VscFolder } from 'react-icons/vsc'
+
+const FolderCard = ({ title, description, fileCount }) => {
+  return (
+    <div>
+      <VscFolder />
+    </div>
+  )
+}
+
+export default FolderCard
