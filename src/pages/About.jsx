50b2c789da37d148ec1df600891ff162838a164c
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2023-07-03 17:21:44 +0100

    Completed about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
 import Container from '../components/Container'
+import developer from '/assets/images/developer.jpeg'
+import supervisor from '/assets/images/supervisor.png'
 
 const About = () => {
-  return <Container>About</Container>
+  return (
+    <Container className={'h-full pt-6 overflow-y-scroll'}>
+      <div className='text-center mb-6'>
+        <div className='text-3xl font-bold text-gray-800 mb-2'>Welcome to GISHUB</div>
+        <p className='max-w-[700px] m-auto'>
+          Create a web-based GIS data source and a vibrant community platform for GIS specialists in Nigeria, fostering
+          collaboration, knowledge sharing, and access to geospatial data for decision-making and sustainable
+          development in the country.
+        </p>
+      </div>
+      <div className='text-center text-3xl font-bold text-gray-800 mb-4'>Meet the Developers</div>
+      <div className='flex justify-between gap-4'>
+        <div className='flex-1 text-center mb-10'>
+          <img src={developer} alt='Developer' className='rounded-full max-w-[200px] m-auto' />
+          <div className='my-2'>
+            <div className='font-medium text-xl'>Adeeyo Joseph Adebiyi</div>
+            <div className='text-sm font-medium text-gray-600'>Student of Federal University of Technology, Minna.</div>
+          </div>
+          <div className='text-sm text-gray-700'>
+            With expertise in Software Development, Remote Sensing, and GIS analysis, I am dedicated to leveraging
+            technology to solve complex problems and contribute to the field of GIS.
+          </div>
+        </div>
+        <div className='flex-1 text-center mb-10'>
+          <img src={supervisor} alt='Developer' className='rounded-full max-w-[200px] m-auto' />
+          <div className='my-2'>
+            <div className='font-medium text-xl'>Dr. Mary Oluwatobi Odekunle</div>
+            <div className='text-sm font-medium text-gray-600'>
+              Lecturer II of Federal University of Technology, Minna.
+            </div>
+          </div>
+          <div className='text-sm text-gray-700'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam, saepe? Libero recusandae distinctio
+            obcaecati. Numquam itaque sequi natus suscipit, id nesciunt atque.
+          </div>
+        </div>
+      </div>
+    </Container>
+  )
 }
 
 export default About
