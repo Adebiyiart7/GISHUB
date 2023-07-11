3ef2ddaf95facf24610f435a1eea3bb23ae018c2
Adeeyo Joseph Adebiyi <adebiyiartworld@gmail.com>
2023-07-11 11:16:38 +0100

    Updated the get user profile contoller

--- api/controllers/user/me.js
+++ api/controllers/user/me.js
@@ -1,7 +1,6 @@
-const Agent = require("../../models/agent");
-const user = require("../../models/user");
-const User = require("../../models/user");
-const { apiResponse } = require("../../utils");
+const user = require('../../models/user')
+const User = require('../../models/user')
+const { apiResponse } = require('../../utils')
 
 /**
  * @route         GET   /api/users/me
@@ -10,32 +9,27 @@ const { apiResponse } = require("../../utils");
  * @params select; e.g ["fullname", "email",]
  */
 const me = async (req, res) => {
-  let { select } = req.query;
+  let { select } = req.query
   if (select) {
-    select = JSON.parse(select);
+    select = JSON.parse(select)
 
     // remove password incase the user adds it
-    const passwordIndex = select.indexOf("password");
+    const passwordIndex = select.indexOf('password')
     if (passwordIndex > -1) {
-      select.splice(passwordIndex, 1);
+      select.splice(passwordIndex, 1)
     }
   }
 
   let user = await User.findById(req.user._id)
-    .select(select || ["-password", "-isBlocked", "-isAdmin"])
-    .lean();
+    .select(select || ['-password', '-isBlocked', '-isAdmin'])
+    .lean()
 
   if (!user) {
-    res.status(404);
-    throw new Error("User not found!");
+    res.status(404)
+    throw new Error('User not found!')
   }
 
-  // If user has agent details the payload should carry hasAgentDetails as true
-  if (await Agent.findOne({ user: req.user._id })) {
-    user.hasAgentDetails = true;
-  }
+  res.status(200).json(apiResponse(res.statusCode, '', user))
+}
 
-  res.status(200).json(apiResponse(res.statusCode, "", user));
-};
-
-module.exports = me;
+module.exports = me
